41d0795ceaea150b0ceeb49285793e2149c11bf8
AmrutaJathar <117621090+AmrutaJathar@users.noreply.github.com>
2022-11-09 11:51:10 +0530

    Create pro.cpp

---
+++ pro.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+  
+  int num, count=0;
+
+  
+  cout << "Enter a Positive Integer: ";
+  cin >> num;
+  
+  
+  if(num<=1) count++;
+  
+  
+  for (int i=2; i <= num/2; i++)
+  {
+    if (num % i == 0){
+        count++;
+        break;
+    }
+  }
+
+  
+  if (count == 0)
+     cout << "Prime Number." << endl;
+  else
+     cout << "Not a Prime Number." << endl;
+
+  return 0;
+}
